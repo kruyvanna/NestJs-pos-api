d46901c7cb27ef7ea5ea7c19c4d880c5cc53b1c6
Kruy Vanna <kruyvanna@gmail.com>
2022-08-23 12:08:21 +0700

    Product schema. add more props

--- src/product/schema/product.schema.ts
+++ src/product/schema/product.schema.ts
@@ -4,10 +4,24 @@ export type ProductDocument = Product & Document;
 @Schema({ timestamps: true })
 export class Product {
   @Prop({ required: true })
+  code: string;
+
+  @Prop({ required: true })
   name: string;
+
   @Prop()
   cost: number;
+
   @Prop({ required: true })
-  code: string;
+  costPerItem: number;
+
+  @Prop({ required: true })
+  pricePerItem: number;
+
+  @Prop({ required: true })
+  numberOfItemsInTheBox: number;
+
+  @Prop({ required: true })
+  pricePerBox: number;
 }
 export const ProductSchema = SchemaFactory.createForClass(Product);
